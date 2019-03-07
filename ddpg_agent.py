cfafc3c536301e41ef8ee6fd55a50f98e2c813b1
klee <ulamaca.lee@gmail.com>
2019-03-07 15:26:52 +0800

    UPDATE: ddpg_agent.py: decouple target update and learning

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -1,16 +1,13 @@
 import numpy as np
 import random
 import copy
-from collections import namedtuple, deque
-
 from model import Actor, Critic
-
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
 # todo, check hyperparam
-TAU = 1e-2              # for soft update of target parameters
+TAU = 1e-3              # for soft update of target parameters
 WEIGHT_DECAY = 0.0      # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -50,9 +47,6 @@ class Agent():
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
-        # todo, check memory is deleted
-        # todo, check self.step is deleted
-
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
         state = torch.from_numpy(state).float().to(device)
@@ -62,7 +56,7 @@ class Agent():
         self.actor_local.train()
         if add_noise:
             action += self.noise.sample()
-        return action #todo: to see if np.clip(-1,1) is needed
+        return action
 
     def reset(self):
         self.noise.reset()
@@ -83,23 +77,17 @@ class Agent():
         # print("state shape (in learn)", states.shape)
         # print("action shape (in learn)", actions.shape)
         # print("rewards shape (in learn)", rewards.shape)
+        # print("rewards shape (in learn)", rewards)
         # print("next_states (in learn)", next_states.shape)
         # print("dones (in learn)", dones.shape)
+        # print("dones (in learn)", dones)
         # print("a_pred_loc (in learn)", actions_pred_loc.shape)
         # print("next_a_pred_tar (in learn)", next_actions_pred_tar.shape)
 
         # ---------------------------- update critic ---------------------------- #
-        # Get predicted next-state actions and Q values from target models
-        #actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, next_actions_pred_tar)
-        # Compute Q targets for current states (y_i)
-        # print("rewaerd shape: ", rewards.shape)
-        # print("dones shape:", dones.shape)
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
-        # Compute critic loss
         Q_expected = self.critic_local(states, actions)
-        #print("shape Q_tar", Q_targets.shape)
-        #print("shape Q_exp", Q_expected.shape)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
         # Minimize the loss
         self.critic_optimizer.zero_grad()
@@ -110,33 +98,33 @@ class Agent():
         # Compute actor loss
         #
         actor_loss = -self.critic_local(states, actions_pred_loc).mean()
-
-        # test = -self.critic_local(states, next_actions_pred)
-        # print(test.shape)
-
         # Minimize the loss
         self.actor_optimizer.zero_grad()
-        # todo, debug
-        #actor_loss.backward(retain_graph=True) # not sure if this is correct?
         actor_loss.backward()
         self.actor_optimizer.step()
 
+    def targets_update(self, tau):
+        """
+        update actor/critic's target networks
+        :return:
+        """
         # ----------------------- update target networks ----------------------- #
-        self.soft_update(self.critic_local, self.critic_target, TAU)
-        self.soft_update(self.actor_local, self.actor_target, TAU)                     
+        self.soft_update(self.critic_local, self.critic_target, tau)
+        self.soft_update(self.actor_local, self.actor_target, tau)
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
         θ_target = τ*θ_local + (1 - τ)*θ_target
-
         Params
         ======
             local_model: PyTorch model (weights will be copied from)
             target_model: PyTorch model (weights will be copied to)
-            tau (float): interpolation parameter 
+            tau (float): interpolation parameter
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
@@ -144,6 +132,7 @@ class OUNoise:
     def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
+        self.size=size
         self.theta = theta
         self.sigma = sigma
         self.seed = random.seed(seed)
@@ -156,7 +145,7 @@ class OUNoise:
     def sample(self):
         """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(self.size)
         self.state = x + dx
         return self.state
 
