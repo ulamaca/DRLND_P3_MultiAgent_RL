a47fedb74c4dbc4d255988c96bf845ee831eb2a7
klee <ulamaca.lee@gmail.com>
2019-03-10 14:59:41 +0800

    ADD: new model type: cirtic taking processed states with actions

--- model.py
+++ model.py
@@ -13,8 +13,8 @@ class Actor(nn.Module):
     """Actor (Policy) Model."""
 
     def __init__(self, state_size, action_size, seed,
-                 fc1_units=400,
-                 fc2_units=300):
+                 fc1_units=256,
+                 fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -47,7 +47,7 @@ class Critic(nn.Module):
     """Critic (Value) Model."""
 
     def __init__(self, state_size, action_size, seed,
-                 fc1_units=256, fc2_units=128):
+                 fcs1_units=256, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
@@ -59,19 +59,19 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size+action_size, fc1_units)
-        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = torch.cat((state, action), dim=1)
-        x = F.relu(self.fc1(xs))
-        x = F.relu(self.fc2(x))
+        xs = F.elu(self.fcs1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.elu(self.fc2(x))
         return self.fc3(x)
