97800c8a5692e32d75a06e41fbcc590bc1edc139
klee <ulamaca.lee@gmail.com>
2019-03-12 20:29:53 +0800

    test: add working version inspired by Amit Patel's work, one key finding is that proper ddpg initialization (set initial target/local net to be the same) is essential for agent to work; however, some data processing problem might still exist in my implementation, suspecting in the learn part

--- maddpg_agent.py
+++ maddpg_agent.py
@@ -1,8 +1,7 @@
 import numpy as np
 import random
-import copy
 from collections import namedtuple, deque
-from ddpg_agent import Agent as DDPGAgent
+from maddpg_agent_ref import DDPG as DDPGAgent
 import torch
 
 
@@ -11,10 +10,11 @@ BUFFER_SIZE = int(3e4)  # replay buffer size
 BATCH_SIZE = 256       # minibatch size
 GAMMA = 0.99            # discount factor
 LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-3        # learning rate of the critic
-LEARN_EVERY = 2         # updating frequency
-UPDATES_PER_LEARN = 1   # learning steps per learning step
-
+LR_CRITIC = 3e-4        # learning rate of the critic
+LEARN_EVERY = 1         # updating frequency
+UPDATES_PER_LEARN = 3   # learning steps per learning step
+EPISODES_BEFORE_LEARNING = 300
+TAU = 1e-3              # for soft update of target parameters
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
@@ -23,7 +23,7 @@ def xenv_to_xmem(xenv):
         input format of [n_agents, ds] in np.array
         output format of [1, n_agents*ds] in np.array
     """
-    return xenv.reshape((-1))
+    return np.reshape(xenv, newshape=(-1))
 
 
 def xenv_to_oi(xenv, ith_agent):
@@ -32,7 +32,7 @@ def xenv_to_oi(xenv, ith_agent):
         output format of [1, ds] in np.array
     """
     #return xenv[np.newaxis, ith_agent, :]
-    return np.reshape(xenv[ith_agent,:], newshape=(1,-1)
+    return np.reshape(xenv[ith_agent,:], newshape=(1,-1))
 
 
 def xmem_to_oi(xmem, ith_agent, state_size):
@@ -43,11 +43,11 @@ def xmem_to_oi(xmem, ith_agent, state_size):
     start = ith_agent * state_size
     end=start+state_size
     if xmem.shape[0] == 1:
-        return xmem[np.newaxis, start:end]
+        return xmem[:, start:end].unsqueeze(0)
     elif xmem.shape[0] > 1:
         return xmem[:, start:end]
     else:
-        raise ValueError("xmem has incorrect format")
+        raise ValueError("xmem has incorrect format, should have at least 2 dimensions")
 
 class MADDPGAGENT():
     """Interacts with and learns from the environment."""
@@ -62,100 +62,92 @@ class MADDPGAGENT():
             random_seed (int): random seed
         """
         self.n_agents = n_agents
-        self.agents = dict([(i, None) for i in range(n_agents)])
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(random_seed)
         self.t_step = 0
-
-        for i in range(n_agents):
-            self.agents[i] = DDPGAgent(state_size, action_size, n_agents, lr_a, lr_c, random_seed)
+        self.agents = dict([(i, None) for i in range(n_agents)])
+        for i in range(self.n_agents):
+            self.agents[i]=DDPGAgent(state_size, action_size, n_agents)
 
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
-    
-    def step(self, xenv, action, reward, next_state, done):
-        """Save experience in replay memory, and use random sample from buffer to learn."""
-        # Save experience / reward
-        self.memory.add(xenv,
-                        xenv_to_xmem(xenv),
-                        xenv_to_xmem(action),
-                        reward,
-                        next_state,
-                        xenv_to_xmem(next_state),
-                        done)
-
-        # Learn, if enough samples are available in memory
-        self.t_step = (self.t_step + 1) % LEARN_EVERY
-        # print("t_step: ", self.t_step)
-
-        # print("learning happening")
-        if self.t_step == 0:
-            if len(self.memory) > BATCH_SIZE:
-                for _ in range(UPDATES_PER_LEARN):
-                    for ith_agent in range(self.n_agents):
-                        experiences = self.memory.sample()
-                        self.learn(experiences, GAMMA, ith_agent)
-
-    def single_agent_act(self, o_i, ith_agent, add_noise=True):
-        """return actions of all agents"""
-        return self.agents[ith_agent].act(o_i, add_noise=add_noise)
-
-    def multi_agents_act(self, xenv, add_noise=True):
-        """return actions of all agents"""
-        actions=[]
-        for ith_agent in range(self.n_agents):
-            o_i=xenv_to_oi(xenv, ith_agent)
-            action=self.single_agent_act(o_i, ith_agent, add_noise=add_noise)
-            action=np.reshape(action, newshape=(1, -1))
-
-            print("final action shape:", action.shape)
-            print(A)
-
-            actions.append(action)
-        actions = np.concatenate(actions, axis=0)
-        return actions
 
     def reset(self):
         for i in range(self.n_agents):
             self.agents[i].reset()
 
-    def learn(self, experiences, gamma, ith_agent):
-        """Update policy and value parameters using given batch of experience tuples.
-        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
-        where:
-            actor_target(state) -> action
-            critic_target(state, action) -> Q-value
+    def step(self, state, action, reward, next_state, done, n_episode):
+        """Save experience in replay memory, and use random sample from buffer to learn."""
+        # Save experience / reward
+        self.memory.add(xenv_to_xmem(state),
+                        state,
+                        action,
+                        reward,
+                        xenv_to_xmem(next_state),
+                        next_state,
+                        done)
 
-        Params
-        ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
-            gamma (float): discount factor
-        """
+        # Learn, if enough samples are available in memory
+        self.t_step = (self.t_step + 1) % LEARN_EVERY
 
-        #v3
-        xs, xmems, states, actions, rewards, next_states, next_xmems, dones = experiences
+        if self.t_step == 0:
+            if len(self.memory) > BATCH_SIZE and n_episode > EPISODES_BEFORE_LEARNING:
+                for _ in range(UPDATES_PER_LEARN):
+                    for ith_agent in range(self.n_agents):
+                        experiences = self.memory.sample()
+                        self.learn(experiences, GAMMA, ith_agent)
+                    self.multi_agents_target_update(tau=TAU)
 
-        # 1. predict action from policy
-        next_as_pred_targ = torch.zeros(states.shape[:2] + (self.action_size,), dtype=torch.float, device=device)
-        for jth_agent in range(self.n_agents): # j for another iterator for n_agents
-            # 2. for next_as_pred_targ (for critic learning):
-            next_o_is = next_states[:, ith_agent, :]
-            # todo, a=(a1,a2,...mu(oi),...aN)
-            next_as_pred_targ[:, jth_agent, :] = self.agents[jth_agent].actor_target(next_o_is) # todo, if to detach??, using target net for value only
+    def multi_agents_act(self, states, n_episode, add_noise=True):
+        """return actions of all agents"""
+        actions = []
+        for agent_id, agent in self.agents.items():
+            o_i=xenv_to_oi(states, agent_id)
+            action = agent.act(o_i, n_episode, add_noise)
+            actions.append(action)
+        actions = np.concatenate(actions, axis=0)
+        return actions
 
-        agent_state = states[:,ith_agent,:]
-        as_pred_loc = actions.clone() #create a deep copy
-        as_pred_loc[:,ith_agent,:] = self.agents[ith_agent].actor_local.forward(agent_state)
-        as_pred_loc = as_pred_loc.view(-1, self.action_size*self.n_agents)
+    # def multi_agents_act(self, states, n_episode, add_noise=True):
+    #     """return actions of all agents"""
+    #     actions = []
+    #     for agent_id, agent in enumerate(self.agents):
+    #         action = agent.act(np.reshape(states[agent_id, :], newshape=(1, -1)), n_episode, add_noise)
+    #         #action = np.reshape(action, newshape=(1, -1)), not necessary
+    #         actions.append(action)
+    #     actions = np.concatenate(actions, axis=0)
+    #     return actions
 
-        # 2. learning
-        single_ddpg_exps=(xmems, actions,
-                          rewards[:, ith_agent].view(-1, 1),
-                          next_xmems,
-                          dones[:, ith_agent].view(-1, 1))
+    def multi_agents_target_update(self, tau):
+        for ith_agent in range(self.n_agents):
+            self.agents[ith_agent].soft_update_all()
 
-        self.agents[ith_agent].learn(single_ddpg_exps, gamma,
-                                     as_pred_loc, next_as_pred_targ)
+    def learn(self, samples, gamma, agent_no):
+        # for learning MADDPG
+        full_states, states, actions, rewards, full_next_states, next_states, dones = samples
+
+        whole_action_dim=self.action_size*self.n_agents
+
+        # todo, if it works, to see what is different between agnet_id and agent_no
+        critic_full_next_actions = torch.zeros(states.shape[:2] + (self.action_size,), dtype=torch.float, device=device)
+        for agent_id in range(self.n_agents):
+            agent_next_state = next_states[:, agent_id, :]
+            critic_full_next_actions[:, agent_id, :] = self.agents[agent_id].actor_target.forward(agent_next_state)
+        critic_full_next_actions = critic_full_next_actions.view(-1, whole_action_dim)
+
+        agent = self.agents[agent_no]
+        agent_state = states[:, agent_no, :]
+        actor_full_actions = actions.clone()  # create a deep copy
+        actor_full_actions[:, agent_no, :] = agent.actor_local.forward(agent_state)
+        actor_full_actions = actor_full_actions.view(-1, whole_action_dim)
+
+        full_actions = actions.view(-1, whole_action_dim)
+
+        agent_rewards = rewards[:, agent_no].view(-1, 1)  # gives wrong result without doing this
+        agent_dones = dones[:, agent_no].view(-1, 1)  # gives wrong result without doing this
+        experiences = (full_states, actor_full_actions, full_actions, agent_rewards, \
+                       agent_dones, full_next_states, critic_full_next_actions)
+        agent.learn(experiences, gamma)
 
 
 class ReplayBuffer:
@@ -184,12 +176,15 @@ class ReplayBuffer:
         experiences = random.sample(self.memory, k=self.batch_size)
 
         xs = torch.from_numpy(np.array([e.x for e in experiences if e is not None])).float().to(device)
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        states = torch.from_numpy(np.array([e.state for e in experiences if e is not None])).float().to(device)
+        print(xs[0:2,24:29])
+        print(states[0:2,1, :5])
+        print(A)
+        actions = torch.from_numpy(np.array([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.array([e.reward for e in experiences if e is not None])).float().to(device)
         next_xs = torch.from_numpy(np.array([e.next_x for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
+        next_states = torch.from_numpy(np.array([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.array([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
             device)
 
         return (xs, states, actions, rewards, next_xs, next_states, dones)
@@ -197,4 +192,3 @@ class ReplayBuffer:
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
-
