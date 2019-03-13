2170ae8509413edbf621592ca776784921ea010a
klee <ulamaca.lee@gmail.com>
2019-03-13 21:03:01 +0800

    refactor: remove feeding i_episode information to the agent (i in the main loop), planning to make the noise decay happen within the agent only

--- run.py
+++ run.py
@@ -5,13 +5,20 @@ from maddpg_agent import MADDPGAGENT
 import time
 import os
 
-n_epsisodes=20000
+n_epsisodes=5000
 training=True
 seed = 10
 print_every = 100
 state_size=24
 action_size=2
 num_agents=2
+
+
+# def seeding(seed=10):
+#     random.seed(seed)
+#     np.random.seed(seed)
+#     torch.manual_seed(seed)
+
 
 if __name__ == "__main__":
     agent = MADDPGAGENT(state_size=state_size, action_size=action_size, random_seed=seed, n_agents=num_agents)
@@ -23,12 +30,12 @@ if __name__ == "__main__":
     for i in range(n_epsisodes):
         states = env.reset()
         scores = np.zeros(num_agents)                          # initialize the score (for each agent)
-        agent.reset()                                          # reset all noise mu_i
+        #agent.reset()                                          # reset all noise mu_i
         while True:
-            actions = agent.multi_agents_act(states, i)           # select an action (for each agent)
+            actions = agent.multi_agents_act(states)           # select an action (for each agent)
             next_states, rewards, dones = env.step(actions)
             scores+=rewards
-            agent.step(states, actions, rewards, next_states, dones, i)
+            agent.step(states, actions, rewards, next_states, dones)
             states = next_states
             if np.any(dones):                                  # exit loop if episode finished
                 break
@@ -38,17 +45,16 @@ if __name__ == "__main__":
         scores_deque.append(episdoe_score)
         mean_score_deque=np.mean(scores_deque)
         print('\rScore (max over agents) from episode {}: {:.4f}'.format(i, episdoe_score), end="")
-        #print('\rAll Scores from episode {}: {:.4f}, {:.4f}'.format(i, scores[0], scores[1]))
         if i>0 and i % print_every==0:
             print('\rMean Score (max over agents) from episode {}: {:.4f}'.format(i, mean_score_deque))
-
         if mean_score_deque > 0.5:
             print("\nProblem Solved!")
             break
     t1=time.time()
+
     print("\nTotal time elapsed: {} seconds".format(t1-t0))
     # save agent params:
-
+    agent.save_params()
     with open(os.path.join('./data/maddpg_test/progress.txt'), 'w') as myfile:
         myfile.write(str(scores_history))
     myfile.close()
